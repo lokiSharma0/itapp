bd5c146e79991bd09df8aac60c259b05ac2b5e4b
luffyJoyboy <lorenzsharma@gmail.com>
2024-09-08 13:18:28 +1000

    newlogowith annimation

--- app/login/_animatedtext.js
+++ app/login/_animatedtext.js
@@ -3,7 +3,7 @@ const Animated_text = () => {
     <div className="absolute bottom-0 w-full overflow-hidden">
       <div className="whitespace-nowrap animate-slide text-center text-gray-600 py-2">
         Welcome to our platform! Enjoy seamless and secure login. Let's make
-        your experience better.
+        your experience better. All the Best
       </div>
     </div>
   );
